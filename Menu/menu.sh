6808eaa21ed4363fce8c511cda67a1a942bc675a
Planet Nine <pl9-fund@pm.me>
2023-01-26 23:57:42 +0300

    Update menu.sh

--- Menu/menu.sh
+++ Menu/menu.sh
@@ -3,6 +3,10 @@
 #X-l1bra  
 clear && source <(curl -s https://raw.githubusercontent.com/dzhager/xl1/main/function/common.sh)
 printLogo
+echo -ne "$(printCyan '                  =====================')
+$(printRed  ' ================')$(printCyan ' =      ')$(printBMagenta 'Добро пожаловать!')$(printCyan '       = ')$(printRed  '================') 
+$(printCyan '                  =====================')"
+}
 mainmenu() { echo -ne "
 
  		     $(printBCyan 'Выберите ноду !')
