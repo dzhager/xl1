ed63cd9100ebf6a04780222093bd6f4475b04bde
Planet Nine <pl9-fund@pm.me>
2023-01-26 23:12:58 +0300

    Update install.sh

--- node/nibiru/install.sh
+++ node/nibiru/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #X-l1bra
-	clear #&& source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/common.sh)
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhager/xl1/main/function/common.sh)
 printLogo
 printnibiru
 echo
