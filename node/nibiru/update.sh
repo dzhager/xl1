fb675350887359579df9284d3c923aaa1915139c
Planet Nine <pl9-fund@pm.me>
2023-01-29 14:19:27 +0300

    Update update.sh

--- node/nibiru/update.sh
+++ node/nibiru/update.sh
@@ -3,16 +3,7 @@
 printLogo
 printnibiru
 echo
-
-# example command:
-#git --version
-# output:
-#git version 2.30.2
-
-# version to compare
 var3=v0.16.3
-
-# get version from application output
 var4=`nibid version | cut -d ' ' -f 3`
 
 if [[ "$var3" == "$var4" ]]; then
