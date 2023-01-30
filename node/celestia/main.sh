2d140d343415e31dd04e28f994c685cf51e49488
Planet Nine <st.siarhei@pm.me>
2023-01-30 18:23:19 +0300

    Обновлены ссылки

--- node/celestia/main.sh
+++ node/celestia/main.sh
@@ -1,1 +1,70 @@
-#!/bin/bash
+#! /bin/bash
+
+#X-l1bra  
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
+printLogo
+printcelestia
+mainmenu() { echo -ne "
+
+		$(printBCyan ' -->') $(printBCyan    '1) Управление')
+
+		$(printBCyan ' -->') $(printBGreen    '2) Установить')
+		$(printBCyan ' -->') $(printBYellow    '3) Обновить')
+		$(printBCyan ' -->') $(printBRed    '4) Удалить')
+
+		$(printBBlue ' <-- 5) Назад')
+		$(printBRed        '     0) Выход')
+
+ 	Введите цифру: "
+
+read -r ans
+	case $ans in
+		1)
+		control
+		;;
+		3)
+		update
+		;;
+		2)
+		install
+		;;
+		4)
+		delet
+		;;
+		5)
+		back
+		;;
+		0)
+		echo $(printBCyan '"Bye bye."')
+		exit
+		;;
+		*)
+		clear
+		printLogo
+		printcelestia
+		echo $(printRed 'Неверный запрос !')
+		;;
+	esac
+}
+
+install(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/celestia/install.sh)
+}
+
+control(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/celestia/control.sh)
+}
+
+update(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/celestia/update.sh)
+}
+
+delet(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/celestia/delet.sh)
+}
+
+back(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/Menu/menu.sh)
+}
+
+mainmenu
