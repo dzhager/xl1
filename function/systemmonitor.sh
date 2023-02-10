b340723e0cae2c0b041adaebaa30a7bdf35196a7
Planet Nine <st.siarhei@pm.me>
2023-02-10 13:03:02 +0300

    Update systemmonitor.sh

--- function/systemmonitor.sh
+++ function/systemmonitor.sh
@@ -21,11 +21,29 @@ echo
 # fi
 
 
- if [[ -n ""btop -l | grep $arg"" ]] #если строка не пуста,
-        then
-        btop #то пакет установлен,
-        else
-        sudo apt update && sudo apt install snapd -y < "/dev/null"
-  		snap install btop
-  		btop
-    fi
+ # if [[ -n ""btop -v | grep $arg"" ]] #если строка не пуста,
+ #        then
+ #        btop #то пакет установлен,
+ #        else
+ #        sudo apt update && sudo apt install snapd -y < "/dev/null"
+ #  		snap install btop
+ #  		btop
+ #    fi
+
+# var5=v0.11.0
+# var6=`btop version | cut -d ' ' -f 3`
+
+# if [[ "$var3" == "$var4" ]]; then
+#  echo -ne "$(printBGreen '	У вас уже установлена последняя версия!')"
+#  mainmenu
+# else
+#  	update
+#  	submenu
+# fi
+
+if ! [ -x "$(command -v btop)" ]; then
+  sudo apt update && sudo apt install snapd -y < "/dev/null"
+  snap install btop
+fi
+
+btop
