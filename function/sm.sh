fece801fb43ee03ea2ebbcc023bde9daa25eea43
Planet Nine <st.siarhei@pm.me>
2023-02-10 13:49:20 +0300

    update

--- function/sm.sh
+++ function/sm.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-echo "Запуск"
 
-sudo apt install snapd -y > /dev/null 2>&1
-snap install btop > /dev/null 2>&1
+mainmenu(){
+    echo -ne "
+$(printYellow    'Для того что бы остановить выйти из системного монитора') $(printBCyan 'q') $(printYellow '!!!')
+
+	Для продолжения нажмите Enter:  "
+	read -r ans
+	case $ans in
+		*)
+		echo -ne "
+	    $(printBCyan ' -->') $(printBYellow    'Запуск.....') "
+		sudo apt install snapd -y > /dev/null 2>&1
+		snap install btop > /dev/null 2>&1
+		btop
+		./x-l1bra
+		;;
+	esac
+}
+
+mainmenu
 
 
-btop
-
-./x-l1bra
