f58679ee33d390c41f807749882b145fb5ea9e9f
Planet Nine <st.siarhei@pm.me>
2023-02-10 14:16:10 +0300

    Update sm.sh

--- function/sm.sh
+++ function/sm.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
-
-mainmenu(){
-    echo -ne "
-$(printYellow    'Для того что бы остановить выйти из системного монитора') $(printBCyan 'q') $(printYellow '!!!')
-
-	Для продолжения нажмите Enter:  "
+printLogo
+echo
+echo
+mainmenu() {
+	echo -ne "
+	$(printYellow    'Для того что бы выйти из системного монитора нажмите') $(printBRed 'q') $(printYellow '!!!')
+	$(printBCyan ' -->') $(printBGreen    'Нажмите') $(printBCyan 'Enter') $(printBGreen 'для запуска')
+	$(printBCyan ' -->') $(printBYellow   'Для установки введите цифру') $(printBCyan '2') $(printBYellow 'и нажмите') $(printBCyan 'Enter')
+	$(printCyan	'Введите цифру:') "
 	read -r ans
 	case $ans in
-		*)
-		echo -ne "
-	    $(printBCyan ' -->') $(printBYellow    'Запуск.....') "
+		2)
+		$(printBCyan ' -->') $(printBYellow    'Установка.....') "
+		sudo apt update  > /dev/null 2>&1
 		sudo apt install snapd -y > /dev/null 2>&1
 		snap install btop > /dev/null 2>&1
 		btop
 		./x-l1bra
 		;;
-	esac
+		*)
+		btop
+		./x-l1bra
+        ;;
+    esac
 }
 
 mainmenu
+
 
 
