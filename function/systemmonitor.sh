aaaacc4a896b57fd7476ea7db2582984142ea7df
Planet Nine <st.siarhei@pm.me>
2023-02-10 13:24:37 +0300

    Update systemmonitor.sh

--- function/systemmonitor.sh
+++ function/systemmonitor.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
-printLogo
 echo "Запуск"
 
-sudo apt install snapd -y  > /dev/null 2>&1
+sudo apt install snapd -y > /dev/null 2>&1
 snap install btop > /dev/null 2>&1
 
 
