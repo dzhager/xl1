80eb2ec6ea6a031d371476099c4392770a9031b6
Planet Nine <st.siarhei@pm.me>
2023-02-15 18:30:56 +0300

    Update delet_new.sh

--- node/celestia/delet_new.sh
+++ node/celestia/delet_new.sh
@@ -1,1 +1,75 @@
-delet_new.sh
+#! /bin/bash
+
+#X-l1bra  
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
+printLogo
+printcelestia
+
+mainmenu() {
+    echo -ne "
+    
+	  $(printBRed    'Вы действительно хотите удалить Celestia ') $(printBRedBlink '!!!')
+	  
+		$(printRed   '1) Да')
+		$(printGreen '2) Нет')
+		
+	  $(printBCyan 'Введите цифру:') "
+	read -r ans
+	case $ans in
+		1)
+		yes
+		;;
+		2)
+		no
+		;;
+
+		*)
+		clear
+		printLogo
+		printcelestia
+		echo
+		echo
+		echo    -ne "$(printRed '		   Неверный запрос !')"
+		echo
+		mainmenu
+		;;
+	esac
+}
+
+no(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/celestia/main.sh)
+}
+
+yes(){
+clear
+printLogo
+printcelestia
+echo -ne "	
+
+$(printBYellow 'Удаляем.....!')"
+cd $HOME
+sudo systemctl stop celestia-appd
+sudo systemctl disable celestia-appd
+sudo rm /etc/systemd/system/celestia-appd.service
+sudo systemctl daemon-reload
+rm -f $(which celestia-appd)
+rm -rf $HOME/.celestia-app
+rm -rf $HOME/celestia-app
+submenu
+}
+
+submenu(){
+	echo -ne "
+	$(printBGreen    'Celestia полностью удалена с вашего сервера ')$(printBGreenBlink '!!!')
+	
+	Нажмите Enter:  "
+	read -r ans
+	case $ans in
+		*)
+		source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/celestia/main.sh)
+		;;
+	esac
+}
+
+
+mainmenu
