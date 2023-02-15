574b5b18340b8c09faec929c8153297c36f9e1c6
Planet Nine <st.siarhei@pm.me>
2023-02-15 19:34:29 +0300

    update

--- node/celestia/install.sh
+++ node/celestia/install.sh
@@ -55,7 +55,7 @@ read -r -p "Enter node moniker:" MONIKER
 
 printBCyan "Пожалуйста подождите........" && sleep 1
 printYellow "1. Oбновляем наш сервер........" && sleep 1
-	sudo apt update > /dev/null 2>&1
+	sudo apt update && sudo apt upgrade > /dev/null 2>&1
 printGreen "Готово!" && sleep 1
 
 
@@ -79,70 +79,90 @@ printGreen "Готово!" && sleep 1
 
 
 printYellow "4. Устанавливаем go........" && sleep 1
-	source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/scripts/go_1.19.4.sh)
-echo "$(go version)"
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.19.5.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 printGreen "Готово!" && sleep 1
 
 
 printYellow "5. Скачиваем и устанавливаем бинарник........"
-	cd $HOME || return
+	cd $HOME
 	rm -rf celestia-app
 	git clone https://github.com/celestiaorg/celestia-app.git
-	cd celestia-app || return
+	cd celestia-app
 	git checkout v0.11.0
-	make install
-	celestia-appd version # 0.11.0
+	make build
+	mkdir -p $HOME/.celestia-app/cosmovisor/genesis/bin
+	mv build/celestia-appd $HOME/.celestia-app/cosmovisor/genesis/bin/
+	rm -rf build
+	ln -s $HOME/.celestia-app/cosmovisor/genesis $HOME/.celestia-app/cosmovisor/current
+	sudo ln -s $HOME/.celestia-app/cosmovisor/current/bin/celestia-appd /usr/local/bin/celestia-appd
+printGreen "Готово!" && sleep 1
+
+printYellow "Устанавливаем cosmovisor и создаем сервис........"
+	go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+	sudo tee /etc/systemd/system/celestia-appd.service > /dev/null << EOF
+	[Unit]
+	Description=celestia-testnet node service
+	After=network-online.target
+
+	[Service]
+	User=$USER
+	ExecStart=$(which cosmovisor) run start
+	Restart=on-failure
+	RestartSec=10
+	LimitNOFILE=65535
+	Environment="DAEMON_HOME=$HOME/.celestia-app"
+	Environment="DAEMON_NAME=celestia-appd"
+	Environment="UNSAFE_SKIP_BACKUP=true"
+
+	[Install]
+	WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable celestia-appd
+
 printGreen "Готово!" && sleep 1
 
 
 printYellow "6. Инициализируем ноду........" && sleep 1
-	celestia-appd config keyring-backend test
-	celestia-appd config chain-id $CHAIN_ID
-	celestia-appd init "$MONIKER" --chain-id $CHAIN_ID
-	curl -s https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/genesis.json > $HOME/.celestia-app/config/genesis.json
-	curl -s https://snapshots3-testnet.nodejumper.io/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json
+celestia-appd config chain-id mocha
+celestia-appd config keyring-backend test
+celestia-appd config node tcp://localhost:20657
+celestia-appd init $MONIKER --chain-id mocha
+curl -Ls https://snapshots.kjnodes.com/celestia-testnet/genesis.json > $HOME/.celestia-app/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json
 printGreen "Готово!" && sleep 1
 
 
 printYellow "7. Добавляем сиды и пиры........" && sleep 1
-SEEDS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/seeds.txt | tr -d '\n')
-PEERS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/peers.txt | tr -d '\n')
-	sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.celestia-app/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@celestia-testnet.rpc.kjnodes.com:20659\"|" $HOME/.celestia-app/config/config.toml
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.005utia\"|" $HOME/.celestia-app/config/app.toml
 printGreen "Готово!" && sleep 1
 
 printYellow "8. Настраиваем прунинг........" && sleep 1
-PRUNING_INTERVAL=$(shuf -n1 -e 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)
-	sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.celestia-app/config/app.toml
-	sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.celestia-app/config/app.toml
-	sed -i 's|^pruning-interval *=.*|pruning-interval = "'$PRUNING_INTERVAL'"|g' $HOME/.celestia-app/config/app.toml
-	sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' $HOME/.celestia-app/config/app.toml
-	sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001utia"|g' $HOME/.celestia-app/config/app.toml
-	sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.celestia-app/config/config.toml
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+  $HOME/.celestia-app/config/app.toml
 printGreen "Готово!" && sleep 1
 
 
-printYellow "9. Cоздаем сервис файл........"
-sudo tee /etc/systemd/system/celestia-appd.service > /dev/null << EOF
-[Unit]
-Description=Celestia Validator Node
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$(which celestia-appd) start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
-[Install]
-WantedBy=multi-user.target
-EOF
-printGreen "Готово!" && sleep 1
+printYellow "10. Устанавливаем кастомные порты........"
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:20658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:20657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:20060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:20656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":20660\"%" $HOME/.celestia-app/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:20317\"%; s%^address = \":8080\"%address = \":20080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:20090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:20091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:20545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:20546\"%" $HOME/.celestia-app/config/app.toml
+printGreen "Готово." && sleep 1
+
+printYellow "11. Подгружаем последний снапшот........"
+curl -L https://snapshots.kjnodes.com/celestia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app
+[[ -f $HOME/.celestia-app/data/upgrade-info.json ]] && cp $HOME/.celestia-app/data/upgrade-info.json $HOME/.celestia-app/cosmovisor/genesis/upgrade-info.json
+printGreen "Готово."
 
 
-#printYellow "11. Подгружаем последний снапшот........"
-#celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app --keep-addr-book
-#SNAP_NAME=$(curl -s https://snapshots3-testnet.nodejumper.io/celestia-testnet/ | egrep -o ">mocha.*\.tar.lz4" | tr -d ">")
-#curl https://snapshots3-testnet.nodejumper.io/celestia-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.celestia-app
-#printGreen "Готово."
 
 printYellow "10. Запускаем ноду........" && sleep 2
 sudo systemctl daemon-reload
@@ -165,7 +185,7 @@ submenu(){
 echo -ne "
 $(printGreen    'Установка завершена.')
 		1) Просмотреть логи
-		2) Проверить синхронизацию
+		2) Проверить статус сервиса
 		3) В меню
 Нажмите Enter:  "
 	read -r ans
@@ -176,7 +196,8 @@ $(printGreen    'Установка завершена.')
 
 		2)
 		echo
-		curl -s localhost:26657/status
+		echo
+		sudo systemctl status celestia-appd
 		submenu
 		;;
 
