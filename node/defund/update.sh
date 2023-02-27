696a202a6d6931787cf288b8800cd16ac728c792
Planet Nine <st.siarhei@pm.me>
2023-02-27 13:30:08 +0300

    Update update.sh

--- node/defund/update.sh
+++ node/defund/update.sh
@@ -1,56 +1,57 @@
 #! /bin/bash
-	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
-printLogo
-printdefund
-echo
-var3=0.2.4
-var4=`defundd version | cut -d ' ' -f 3`
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
+# 	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
+# printLogo
+# printdefund
+# echo
+# var3=0.2.4
+# var4=`defundd version | cut -d ' ' -f 3`
 
-if [[ "$var3" == "$var4" ]]; then
- echo -ne "$(printBGreen '	У вас уже установлена последняя версия!')"
- mainmenu
-else
- 	update
- 	submenu
-fi
+# if [[ "$var3" == "$var4" ]]; then
+#  echo -ne "$(printBGreen '	У вас уже установлена последняя версия!')"
+#  mainmenu
+# else
+#  	update
+#  	submenu
+# fi
 
 
-update(){
-# Clone project repository
-cd $HOME
-rm -rf defund
-git clone https://github.com/defund-labs/defund.git
-cd defund
-git checkout v0.2.4
+# update(){
+# # Clone project repository
+# cd $HOME
+# rm -rf defund
+# git clone https://github.com/defund-labs/defund.git
+# cd defund
+# git checkout v0.2.4
 
-# Build binaries
-make build
+# # Build binaries
+# make build
 
-# Prepare binaries for Cosmovisor
-mkdir -p $HOME/.defund/cosmovisor/upgrades/v0.2.3/bin
-mv build/defundd $HOME/.defund/cosmovisor/upgrades/v0.2.3/bin/
-rm -rf build
-}
+# # Prepare binaries for Cosmovisor
+# mkdir -p $HOME/.defund/cosmovisor/upgrades/v0.2.3/bin
+# mv build/defundd $HOME/.defund/cosmovisor/upgrades/v0.2.3/bin/
+# rm -rf build
+# }
 
-mainmenu(){
-	echo -ne "
-	$(printBCyan '	Для возврата нажмите Enter:')  "
-		read -r ans
-		case $ans in
-			*)
-			source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
-			;;
-	esac
-}
+# mainmenu(){
+# 	echo -ne "
+# 	$(printBCyan '	Для возврата нажмите Enter:')  "
+# 		read -r ans
+# 		case $ans in
+# 			*)
+# 			source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
+# 			;;
+# 	esac
+# }
 
-submenu(){
-	echo -ne "
-	$(printBGreen '	Обновление завершено!')
-	$(printBCyan '	Для возврата нажмите Enter:')  "
-		read -r ans
-		case $ans in
-			*)
-			source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
-			;;
-	esac
-}
+# submenu(){
+# 	echo -ne "
+# 	$(printBGreen '	Обновление завершено!')
+# 	$(printBCyan '	Для возврата нажмите Enter:')  "
+# 		read -r ans
+# 		case $ans in
+# 			*)
+# 			source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
+# 			;;
+# 	esac
+# }
