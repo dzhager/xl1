ece410146feca8fa99a092a21f7c9df201f4bcc8
Planet Nine <st.siarhei@pm.me>
2023-02-27 11:49:00 +0300

    Update install.sh

--- node/defund/install.sh
+++ node/defund/install.sh
@@ -1,4 +1,210 @@
 #!/bin/bash
 
 #X-l1bra
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
+printLogo
+printdefund
+echo -ne "
+	$(printGreen  '-----------------------------------------')
+	  $(printYellow 'Минимальные требования к оборудованию.')
+		     $(printBCyan '4CPU 8RAM 200GB')
+	$(printGreen  '-----------------------------------------')
+	$(printYellow 'Рекомендуемые требования к оборудованию.')
+		     $(printBCyan '8CPU 15RAM 400GB')
+	$(printGreen  '-----------------------------------------')"
+echo
+mainmenu() {
+	echo -ne "
+	$(printCyan	'Вы действительно хотите начать установку') $(printCyanBlink '???')
+	$(printGreen	' 1) Да')
+	$(printRed	' 2) Нет')
+	$(printCyan	'Введите цифру:') "
+	read -r ans
+	case $ans in
+		1)
+		yes
+		;;
+		2)
+		no
+		;;
+		*)
+		clear
+		printLogo
+		printdefund
+		echo
+		echo
+		echo    -ne "$(printRed '		   Неверный запрос !')"
+		echo
+		mainmenu
+        	;;
+    esac
+}
+
+
+no(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
+}
+yes(){
+clear
+printLogo
+printdefund
+echo
+echo
+read -r -p "  Введите имя ноды:  " MONIKER
+
+
+printBCyan "Пожалуйста подождите........" && sleep 1
+printYellow "1. Oбновляем наш сервер........" && sleep 1
+	sudo apt -q update &&  sudo apt -qy upgrade > /dev/null 2>&1
+printGreen "Готово!" && sleep 1
+
+
+printYellow "2. Устанавливаем дополнительные пакеты........" && sleep 1
+	sudo apt -qy install curl git jq lz4 build-essential
+printGreen "Готово!" && sleep 1
+
+
+printYellow "3. Устанавливаем go........" && sleep 1
+	sudo rm -rf /usr/local/go
+	curl -Ls https://go.dev/dl/go1.19.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+	eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+	eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+printGreen "Готово!" && sleep 1
+
+
+printYellow "4. Скачиваем и устанавливаем бинарник........"
+	cd $HOME
+	rm -rf defund
+	git clone https://github.com/defund-labs/defund.git
+	cd defund
+	git checkout v0.2.4
+	make build
+	mkdir -p $HOME/.defund/cosmovisor/genesis/bin
+	mv build/defundd $HOME/.defund/cosmovisor/genesis/bin/
+	rm -rf build
+	ln -s $HOME/.defund/cosmovisor/genesis $HOME/.defund/cosmovisor/current
+	sudo ln -s $HOME/.defund/cosmovisor/current/bin/defundd /usr/local/bin/defundd
+printGreen "Готово!" && sleep 1
+
+printYellow "5. Устанавливаем cosmovisor и создаем сервис........"
+	go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+sudo tee /etc/systemd/system/defundd.service > /dev/null << EOF
+[Unit]
+Description=defund-testnet node service
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.defund"
+Environment="DAEMON_NAME=defundd"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.defund/cosmovisor/current/bin"
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload && sudo systemctl enable defundd
+printGreen "Готово!" && sleep 1
+
+
+printYellow "6. Инициализируем ноду........" && sleep 1
+defundd config chain-id defund-private-4
+defundd config keyring-backend test
+defundd config node tcp://localhost:40657
+defundd init $MONIKER --chain-id defund-private-4
+printGreen "Готово!" && sleep 1
+
+
+printYellow "7. Добавляем сиды и пиры........" && sleep 1
+curl -Ls https://snapshots.kjnodes.com/defund-testnet/genesis.json > $HOME/.defund/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/defund-testnet/addrbook.json > $HOME/.defund/config/addrbook.json
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@defund-testnet.rpc.kjnodes.com:40659\"|" $HOME/.defund/config/config.toml
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0ufetf\"|" $HOME/.defund/config/app.toml
+printGreen "Готово!" && sleep 1
+
+
+printYellow "8. Настраиваем прунинг........" && sleep 1
+	sed -i \
+-e 's|^pruning *=.*|pruning = "custom"|' \
+-e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+-e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
+-e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+$HOME/.defund/config/app.toml
+printGreen "Готово!" && sleep 1
+
+
+printYellow "9. Устанавливаем кастомные порты........"
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:40658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:40657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:40060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:40656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":40660\"%" $HOME/.defund/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:40317\"%; s%^address = \":8080\"%address = \":40080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:40090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:40091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:40545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:40546\"%" $HOME/.defund/config/app.toml
+printGreen "Готово." && sleep 1
+
+
+printYellow "10. Подгружаем последний снапшот........"
+curl -L https://snapshots.kjnodes.com/defund-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.defund
+[[ -f $HOME/.defund/data/upgrade-info.json ]] && cp $HOME/.defund/data/upgrade-info.json $HOME/.defund/cosmovisor/genesis/upgrade-info.json
+printGreen "Готово."
+
+
+printYellow "11. Запускаем ноду........" && sleep 2
+sudo systemctl start defundd
+printGreen "Готово!"
+
+printRed  =============================================================================== 
+	echo -e "X-l1bra:                   ${CYAN} https://t.me/xl1bra ${NC}"
+printRed  =============================================================================== 
+
+submenu
+
+}
+
+
+
+submenu(){
+echo -ne "
+	$(printBGreen    'УСТАНОВКА ЗАВЕРШЕНА........') $(printBGreenBlink '!!!')
+	
+ 		1) Просмотреть логи
+ 		2) Проверить синхронизацию
+ 		3) В меню
+ 		
+	$(printBCyan 'Нажмите Enter:')  "
+	read -r ans
+	case $ans in
+		1)
+		subsubmenu
+		;;
+		2)
+		echo
+		defundd status 2>&1 | jq .SyncInfo
+		submenu
+		;;
+		3)
+		source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/defund/main.sh)
+		;;
+		*)
+		printLogo
+		printdefund
+		echo
+		echo $(printBRed '	Неверный запрос !!!')
+		submenu
+		;;
+	esac
+}
+
+subsubmenu(){
+	echo -ne "
+	$(printYellow    'Для того что бы остановить журнал логов надо нажать') $(printBCyan 'CTRL+Z') $(printYellow '!!!')
+	$(printBCyan 'Для продолжения нажмите Enter:')  "
+		read -r ans
+		case $ans in
+			*)
+			sudo journalctl -u defundd -f --no-hostname -o cat
+			submenu
+			;;
+	esac
+}
+
+mainmenu
 
