3a3362d953cffa1bfe98ea5e87cab155ef334393
Planet Nine <st.siarhei@pm.me>
2023-03-20 10:58:34 +0300

    Update control.sh

--- node/quasar/control.sh
+++ node/quasar/control.sh
@@ -13,16 +13,18 @@ mainmenu() {
 	    $(printBCyan ' -->') $(printBYellow    '4)') Добавить кошелек wallet
 	    $(printBCyan ' -->') $(printBYellow    '5)') Восстановить кошелек
 	
-	    $(printBCyan ' -->') $(printBYellow    '6)') Делегировать
-	    $(printBCyan ' -->') $(printBYellow    '7)') Делегировать самому себе
-	    $(printBCyan ' -->') $(printBYellow    '8)') Создать валидатора
-	    $(printBCyan ' -->') $(printBYellow    '9)') Узнать информацию о валидаторе
+		$(printBCyan ' -->') $(printBYellow    '6)') Делегировать для x-l1bra
+	    $(printBCyan ' -->') $(printBYellow    '7)') Делегировать кому-то
+	    $(printBCyan ' -->') $(printBYellow    '8)') Делегировать самому себе
+
+	    $(printBCyan ' -->') $(printBYellow    '9)') Создать валидатора
+	    $(printBCyan ' -->') $(printBYellow    '10)') Узнать информацию о валидаторе
 	    
-	    $(printBCyan ' -->') $(printBYellow    '10)') Загрузить последний снапшот
-	    $(printBCyan ' -->') $(printBYellow    '11)') Проверить синхронизацию
-	    $(printBCyan ' -->') $(printBYellow    '12)') Просмотреть логи
+	    $(printBCyan ' -->') $(printBYellow    '11)') Загрузить последний снапшот
+	    $(printBCyan ' -->') $(printBYellow    '12)') Проверить синхронизацию
+	    $(printBCyan ' -->') $(printBYellow    '13)') Просмотреть логи
 	
-	    $(printBBlue ' <--') $(printBBlue    '13) Вернутся назад')
+	    $(printBBlue ' <--') $(printBBlue    '14) Вернутся назад')
 		 $(printBRed    ' 0) Выйти')
 		 
 	$(printCyan 'Введите цифру:')  "
@@ -50,34 +52,38 @@ mainmenu() {
 		;;
 		
 		6)
+		DelegateXl1bra
+		;;
+
+		7)
 		Delegate
 		;;
 		
-		7)
+		8)
 		DelegateYourself
 		;;
 		
-		8)
+		9)
 		CreateValidator
 		;;
 		
-		9)
+		10)
 		InfoValidator
 		;;
 		
-		10)
+		11)
 		snapshot
 		;;
 
-		11)
+		12)
 		synced
 		;;
 		
-		12)
+		13)
 		logs
 		;;
 		
-		13)
+		14)
 		back
 		;;
 		
@@ -139,6 +145,17 @@ clear && printLogo && printquasar
 echo
 quasard keys add wallet --recover
 mainmenu
+}
+
+DelegateXl1bra(){
+	clear && printLogo && printquasar
+	echo
+	echo -ne "$(printBRed ' 1uqs = 1000000uqsr')"
+	echo
+	read -r -p "  Введите количество монет uqsr:  " VAR2
+	quasard tx staking delegate nibivaloper15wvn7z5dyuffs3pv9cnkv69gqtza6ktuprpegn "$VAR2"uqsr --from wallet --chain-id qsr-questnet-04 --gas-adjustment 1.4 --gas auto --gas-prices 0uqsr -y
+	echo
+	mainmenu
 }
 
 Delegate(){
