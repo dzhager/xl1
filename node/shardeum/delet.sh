d120faa414c09ebdfbd88a7c1cfea2813878857b
Planet Nine <st.siarhei@pm.me>
2023-03-20 11:25:40 +0300

    Update delet.sh

--- node/shardeum/delet.sh
+++ node/shardeum/delet.sh
@@ -1,1 +1,69 @@
-#!/bin/bash
+#! /bin/bash
+
+#X-l1bra  
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
+printLogo
+printshardium
+
+mainmenu() {
+    echo -ne "
+    
+	  $(printBRed    'Вы действительно хотите удалить Shardeum ') $(printBRedBlink '!!!')
+	  
+		$(printRed   '1) Да')
+		$(printGreen '2) Нет')
+		
+	  $(printBCyan 'Введите цифру:') "
+	read -r ans
+	case $ans in
+		1)
+		yes
+		;;
+		2)
+		no
+		;;
+
+		*)
+		clear
+		printLogo
+		printshardium
+		echo
+		echo
+		echo    -ne "$(printRed '		   Неверный запрос !')"
+		echo
+		mainmenu
+		;;
+	esac
+}
+
+no(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/main.sh)
+}
+
+yes(){
+clear
+printLogo
+printshardium
+echo -ne "	
+
+$(printBYellow 'Удаляем.....!')"
+cd $HOME
+rm -rf .shardeum
+submenu
+}
+
+submenu(){
+	echo -ne "
+	$(printBGreen    'Shardeum полностью удалена с вашего сервера ')$(printBGreenBlink '!!!')
+	
+	Нажмите Enter:  "
+	read -r ans
+	case $ans in
+		*)
+		source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/main.sh)
+		;;
+	esac
+}
+
+
+mainmenu
