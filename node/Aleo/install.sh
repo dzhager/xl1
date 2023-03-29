3ffc295baa4550a0f44c92caa5e46981d7e074d1
Planet Nine <st.siarhei@pm.me>
2023-03-29 18:42:45 +0300

    Update install.sh

--- node/Aleo/install.sh
+++ node/Aleo/install.sh
@@ -98,4 +98,5 @@ echo
 	RECORD="$VAR3"
 
 	snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 600000 --record "${RECORD}" &&
-	
+
+	mainmenu
