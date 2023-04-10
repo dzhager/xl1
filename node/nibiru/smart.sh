e446e9804a73f820ee4ca8bc94c3da172ef10ef8
Planet Nine <st.siarhei@pm.me>
2023-04-10 10:28:41 +0300

    Update smart.sh

--- node/nibiru/smart.sh
+++ node/nibiru/smart.sh
@@ -7,13 +7,33 @@ clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/fu
 printLogo
 printnibiru
 
-apt install curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev -y
-curl -s https://get.nibiru.fi/! | bash
-nibid version --long | grep -e version -e commit
-nibid config node https://rpc.itn-1.nibiru.fi:443
-nibid config chain-id nibiru-itn-1
-nibid config broadcast-mode block
-nibid config keyring-backend os
-mkdir -p $HOME/wasm && cd $HOME/wasm
-wget https://github.com/NibiruChain/cw-nibiru/raw/main/artifacts-cw-plus/cw20_base.wasm
-nibid tx wasm store $HOME/wasm/cw20_base.wasm --from wallet --gas 8000000 --fees 200000unibi
+
+git clone https://github.com/NibiruChain/cw-nibiru
+
+nibid tx wasm store $HOME/cw-nibiru/artifacts-cw-plus/cw20_base.wasm --from wallet --gas-adjustment 1.2 --gas auto  --fees 80000unibi  -y
+
+nibid q tx $txhash -o json |  jq -r '.raw_log'
+
+read -r -p "  Введите ваш code_id:  " id
+
+read -r -p "  Введите ваше имя токена:  " var1
+
+read -r -p "  Введите имя символа:  " var2
+
+read -r -p "  Введите адрес вашего кошелька:  " var3
+
+INIT='{"name":"$var1","symbol":"$var2","decimals":6,"initial_balances":[{"address":"$var3","amount":"2000000"}],"mint":{"minter":"$var3"},"marketing":{}}'
+
+nibid tx wasm instantiate $id $INIT --from wallet --label "my cw20_base" --gas-adjustment 1.2 --gas auto  --fees 73794unibi --no-admin -y
+
+CONTRACT=$(nibid query wasm list-contract-by-code $id --output json | jq -r '.contracts[-1]')
+
+read -r -p "  Введите адрес кошелька для отправки токенов:  " var4
+
+TRANSFER='{"transfer":{"recipient":"$var4","amount":"50"}}'
+
+nibid tx wasm execute $CONTRACT $TRANSFER --gas-adjustment 1.2 --gas auto --fees 4000unibi --from wallet -y
+
+BALANCE_QUERY='{"balance": {"address": "$var4"}}'
+
+nibid query wasm contract-state smart $CONTRACT "$BALANCE_QUERY" --output json
