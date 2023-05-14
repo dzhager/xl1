943c146ffc14eac0d317d76b98ff6ad6276813ac
Planet Nine <st.siarhei@pm.me>
2023-05-14 21:06:07 +0300

    Update update.sh

--- node/elixir/update.sh
+++ node/elixir/update.sh
@@ -6,6 +6,7 @@ docker kill ev
 docker rm ev
 docker pull elixirprotocol/validator:testnet-2
 docker build . -f Dockerfile -t elixir-validator
+docker run -d --restart unless-stopped --name ev elixir-validator
 
 mainmenu
 
