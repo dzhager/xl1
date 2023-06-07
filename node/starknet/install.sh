f1ec98f88e167ac3c5a6bb95d325b89da547644c
Planet Nine <st.siarhei@pm.me>
2023-06-07 14:22:18 +0300

    add update node

--- node/starknet/install.sh
+++ node/starknet/install.sh
@@ -80,7 +80,7 @@ sudo update-alternatives --config python3
 printGreen "Готово!" && sleep 1
 
 printYellow "5. Клонируем репозиторий с github........" && sleep 1
-git clone --branch v0.5.3 https://github.com/eqlabs/pathfinder.git
+git clone --branch v0.5.6 https://github.com/eqlabs/pathfinder.git
 printGreen "Готово!" && sleep 1
 
 printYellow "6. Создаем виртуальную среду........" && sleep 1
