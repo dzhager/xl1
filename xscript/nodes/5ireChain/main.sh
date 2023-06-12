61daa2293b60d90c22ab769d97d174b09af94a19
Planet Nine <st.siarhei@pm.me>
2023-06-12 18:07:51 +0300

    update

--- xscript/nodes/5ireChain/main.sh
+++ xscript/nodes/5ireChain/main.sh
@@ -2,7 +2,7 @@
 
 #Script written by DZHAGERR for X-libra
 #-----------------------------Подгрузка общих функций и цвета-----------------------------#
-	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/function/common.sh) && printlogo #&& print5ireChain
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/function/common.sh) && printlogo && print5ireChain
 #-----------------------------------------------------------------------------------------#
 
 
