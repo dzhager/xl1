d609de69def61f8ddc4addeb7bcd72c2f4dea03a
Planet Nine <st.siarhei@pm.me>
2023-06-13 14:42:12 +0300

    new file:   xscript/function/sm.sh

---
+++ xscript/function/sm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Script written by DZHAGERR for X-libra
+
+#-----------------------------Подгрузка общих функций и цвета-----------------------------#
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/function/common.sh) && printLogo
+#-----------------------------------------------------------------------------------------#
+
+if [ $(dpkg-query -W -f='${Status}' snapd 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+    echo "$(printBYellow    '     Подождите, идет первоначальная установка!')"
+    sudo apt-get update && sudo apt-get install snapd && snap install btop && btop
+else
+    btop
+fi
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/menu/homemenu.sh)
+
