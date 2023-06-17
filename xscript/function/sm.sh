d671f2a9641de9e0269af32577d6c6118f03e982
Planet Nine <st.siarhei@pm.me>
2023-06-17 22:28:27 +0300

    modified:   xscript/function/sm.sh

--- xscript/function/sm.sh
+++ xscript/function/sm.sh
@@ -9,7 +9,11 @@
     if [ $(dpkg-query -W -f='${Status}' snapd 2>/dev/null | grep -c "ok installed") -eq 0 ];
     then
         echo "$(printBYellow    '     Подождите, идет первоначальная установка!')"
-        sudo apt-get update && sudo apt-get install snapd -y && snap install btop && btop
+        sudo apt-get update
+        sudo apt-get install 
+        snapd -y 
+        snap install btop 
+        btop
     else
         btop
     fi
