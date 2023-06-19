4a8a14087c87d4748f2ff28ce57a06d30e961960
Planet Nine <st.siarhei@pm.me>
2023-06-19 13:29:19 +0300

    new file:   xscript/nodes/active/shardeum/delet.sh 	new file:   xscript/nodes/active/shardeum/install.sh 	new file:   xscript/nodes/active/shardeum/main.sh 	xscript/nodes/active/shardeum/control.sh

---
+++ xscript/nodes/active/shardeum/delet.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#Script written by DZHAGERR for X-libra
+
+#-----------------------------Подгрузка общих функций и цвета-----------------------------#
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/function/common.sh) && printlogo && printshardium
+#-----------------------------------------------------------------------------------------#
+
+
+mainmenu() {
+    echo -ne "
+    
+	  $(printBRed    'Вы действительно хотите удалить Shardeum ') $(printBRedBlink '!!!')
+	  
+		$(printRed   '1) Да')
+		$(printGreen '2) Нет')
+		
+	  $(printBCyan 'Введите цифру:') "
+	read -r ans
+	case $ans in
+		1)
+		yes
+		;;
+		2)
+		no
+		;;
+
+		*)
+		clear
+		printlogo
+		printshardium
+		echo
+		echo
+		echo    -ne "$(printRed '		   Неверный запрос !')"
+		echo
+		mainmenu
+		;;
+	esac
+}
+
+no(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/main.sh)
+}
+
+yes(){
+clear
+printlogo
+printshardium
+echo -ne "	
+
+$(printBYellow 'Удаляем.....!')"
+cd ~/.shardeum
+./cleanup.sh
+cd ~/
+rm -rf .shardeum
+rm installer.sh
+submenu
+}
+
+submenu(){
+	echo -ne "
+	$(printBGreen    'Shardeum полностью удалена с вашего сервера ')$(printBGreenBlink '!!!')
+	
+	Нажмите Enter:  "
+	read -r ans
+	case $ans in
+		*)
+		source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/main.sh)
+		;;
+	esac
+}
+
+
+mainmenu
---
+++ xscript/nodes/active/shardeum/main.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+#Script written by DZHAGERR for X-libra
+
+#-----------------------------Подгрузка общих функций и цвета-----------------------------#
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/function/common.sh) && printlogo && printshardium
+#-----------------------------------------------------------------------------------------#
+
+#
+mainmenu() { echo -ne "
+		$(printBCyan ' -->') $(printBGreen    '1) Управление')
+
+		$(printBCyan ' -->') $(printBGreen    '2) Установить')
+		$(printBCyan ' -->') $(printBYellow   '3) Обновить Validator')
+		$(printBCyan ' -->') $(printBYellow   '4) Обновить CLI/GUI')
+
+		$(printBCyan ' -->') $(printBRed    '5) Удалить')
+
+		$(printBBlue ' <-- 6) Назад')
+		$(printBRed        '     0) Выход')
+
+ 	Введите цифру: "
+
+read -r ans
+	case $ans in
+		1)
+		control
+		;;
+
+		2)
+		install
+		;;
+
+		3)
+		update
+		;;
+
+		4)
+		updatecli
+		;;
+
+		5)
+		delet
+		;;
+
+		6)
+		back
+		;;
+		
+		0)
+		echo $(printBCyan '"Bye bye."')
+		rm x-l1bra
+		exit
+		;;
+		*)
+		clear
+		printLogo
+		printshardium
+		echo
+		echo
+		echo    -ne "$(printRed '		   Неверный запрос !')"
+		echo
+		mainmenu
+		;;
+	esac
+}
+
+install(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/install.sh)
+}
+
+control(){
+	source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/control.sh)
+}
+
+delet(){
+source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/node/shardeum/delet.sh)
+}
+
+back(){
+./x-l1bra
+}
+
+updatecli(){
+	docker exec -i shardeum-dashboard /bin/bash -c "operator-cli update"
+	mainmenu
+}
+
+
+#--------------ОБНОВЛЕНИЕ 1.1.6
+update(){
+	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/function/common.sh)
+	printLogo
+	printshardium
+	cd $HOME
+	curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
+	echo -ne "
+			    $(printBGreen    'Обновление завершено!')
+			    Далее нужно выйти из скрипта и выполнить команды по очереди:
+			    
+			    $(printBYellow 'cd .shardeum && ./shell.sh')
+			    
+			    $(printBYellow 'operator-cli gui start')
+			    
+			    $(printBYellow 'operator-cli start')
+
+			    Проверить работу ноды можно командой:
+			    $(printBYellow 'pm2 list')"
+
+	mainmenu
+
+}
+
+
+mainmenu
