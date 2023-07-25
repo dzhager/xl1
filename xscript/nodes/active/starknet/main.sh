02a882d911601c9e7b7792f2925b6fa568098dda
Planet Nine <st.siarhei@pm.me>
2023-07-25 11:13:53 +0300

    modified:   xscript/nodes/active/starknet/main.sh

--- xscript/nodes/active/starknet/main.sh
+++ xscript/nodes/active/starknet/main.sh
@@ -75,7 +75,7 @@ rustup update stable
 sudo systemctl stop starknetd
 cd pathfinder
 git fetch
-git checkout v0.6.1
+git checkout v0.6.7
 cargo build --release --bin pathfinder
 cd py
 source .venv/bin/activate
