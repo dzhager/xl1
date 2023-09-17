766112f002519a97f15807eb1ec04609fbd54eb8
DZHAGERR <st.siarhei@pm.me>
2023-09-17 23:41:18 +0300

    modified:   xscript/menu/homemenu.sh

--- xscript/menu/homemenu.sh
+++ xscript/menu/homemenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Script written by DZHAGERR for X-libra
+
 
 #-----------------------------Подгрузка общих функций и цвета-----------------------------#
 	clear && source <(curl -s https://raw.githubusercontent.com/dzhagerr/xl1/main/xscript/function/common.sh) && printlogo
